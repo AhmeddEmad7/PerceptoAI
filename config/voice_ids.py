708270e1e22b27f01c3c4e4f54cf3d4a54e8bad4
Ahmad mahmoud mohamed fouad <am697555@gmail.com>
2025-06-10 04:49:55 +0300

    Add ElevenLabs voice IDs to config

---
+++ config/voice_ids.py
@@ -0,0 +1,16 @@
+ELEVENLABS_VOICE_IDs = {
+         "Rachel": "21m00Tcm4TlvDq8ikWAM",
+         "Domi": "AZnzlk1XvdvUeBnXmlld",
+         "Bella": "EXAVITQu4vr4xnSDxMaL",
+         "Antoni": "ErXwobaYiN019PkySvjV",
+         "Elli": "MF3mGyEYCl7XYWbV9V6O",
+         "Josh": "TxGEqnHWrfWFTfGW9XjX",
+         "Arnold": "VR6AewLTigWG4xSOukaG",
+         "Clyde": "2EiwWnXFnvU5JabPnv8n",
+         "Charlotte": "XB0fDUnXU5powFXDhCwa",
+         # for the best Arabic performance:
+         "Sarah": "EXAVITQu4vr4xnSDxMaL",
+         "Laura": "FGY2WhTYpPnrIDTdsKH5",
+         "Brian": "nPczCjzI2devNBz1zQrb",
+         "Bill": "pqHfZKP75CvOlQylNhV4"
+}
